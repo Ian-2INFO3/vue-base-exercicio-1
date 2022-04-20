695f6b8ffdaed9b6c9b78f4eee8052623bf77b4f
Ian-2INFO3 <ianrobertoestudo10@gmail.com>
2022-04-20 15:01:09 -0300

    github

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script>
 import MenuLateral from "@/components/MenuLateral.vue";
+import Rodape from "@/components/RodaPe.vue";
+import Conteudo from "@/components/ConteNt.vue";
+import Cabecalho from "@/components/CabeCalho.vue";
+import Bloco from "@/components/Bloco.vue";
 export default {
-  components: { MenuLateral },
+  components: { MenuLateral, Rodape, Conteudo, Cabecalho, Bloco },
 };
 </script>
 <template>
   <MenuLateral />
-  <header id="header">Cabeçalho</header>
-  <main id="content">Principal</main>
-  <footer id="footer">Rodapé</footer>
+  <Cabecalho />
+  <Rodape />
+  <Bloco />
+  <Conteudo />
 </template>
 <style></style>
